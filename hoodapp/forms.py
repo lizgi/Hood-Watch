d875d99999d97e3d84b24d10cd70262232afeff0
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-12-21 16:11:25 +0300

    profile form

---
+++ hoodapp/forms.py
@@ -0,0 +1,9 @@
+from .models import Profile
+from django.forms import ModelForm
+from django import forms
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user'] 
