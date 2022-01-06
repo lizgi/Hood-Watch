39f886b2e4af79ecb33c4165929745e17cb3be8e
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2022-01-06 21:28:28 +0300

    register classes in admin

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Business, Location, Neighborhood, Post, Profile
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Location)
+admin.site.register(Neighborhood)
+admin.site.register(Post)
+admin.site.register(Business)
