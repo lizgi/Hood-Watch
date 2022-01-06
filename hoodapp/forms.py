fdfdfc0d4b68b82bbcb314bc878c2d46b0243aba
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2022-01-06 21:33:35 +0300

    add hoodform,bsform and postform

--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -1,4 +1,4 @@
-from .models import Profile
+from .models import Business, Location, Neighborhood, Post, Profile
 from django.forms import ModelForm
 from django import forms
 
@@ -12,4 +12,23 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('profile_pic','bio','contact') 
+
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model=Neighborhood
+        fields = ['hood_image','name','description','occupants_count','location']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighborhood')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
+
+    
 
